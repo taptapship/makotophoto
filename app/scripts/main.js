d0be8b8f9fab649900c6f6373ea0a1cb1e037722
Stephen Sawchuk <sawchuk@gmail.com>
2014-03-21 22:55:05 -0400

    ok

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+angular.
+  module('makoto', ['ui.router']).
+  config(function ($stateProvider, $urlRouterProvider) {
+    'use strict';
+
+    $stateProvider.
+      state('home', {
+        url: '/',
+        templateUrl: 'views/index.html'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  });
